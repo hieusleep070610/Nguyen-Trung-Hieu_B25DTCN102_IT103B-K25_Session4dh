81d5e664bb6690cade3c20a706b72461b0272423
hieusleep070610 <hieusleep070610@gmail.com>
2026-02-02 13:36:44 +0700

    Add files via upload

---
+++ Session 4-dh/cau1.js
@@ -0,0 +1,24 @@
+//Cau 1
+let n = 10;
+
+if(n % 2 == 0){
+    console.log(`Số ${n} là số chẵn`);
+} else {
+    console.log(`Số ${n} là số lẻ`);
+}
+if(n> 0){
+    console.log(`Số ${n} là số dương`);
+} else if ( n < 0){
+    console.log(`Số ${n} là số âm`);
+} else {
+    console.log(`Số ${n} là số không`)
+}
+
+if( n > 0 ){
+    for(let i = 1; i <= n;i++){
+        console.log(`${i}`);
+    }
+} else {
+    console.log(`Giá trị n không hợp lệ để tạo dãy số`)
+}
+
---
+++ Session 4-dh/cau2.js
@@ -0,0 +1,16 @@
+let n = 50;
+let sumFizz = 0;
+for(i = 1; i<= 50;i++){
+    if( i % 3 == 0 && i % 5 == 0){
+        console.log(`FizzBuzz`);
+        sumFizz += i;
+    } else if (i % 5 == 0){
+        console.log(`Buzz`);
+    } else if (i % 3 == 0){
+        console.log(`Fizz`);
+        sumFizz += i;
+    } else {
+        console.log(`${i}`);
+    }
+}
+console.log(`Tổng các số chia hết cho 3 là ${sumFizz}`);
