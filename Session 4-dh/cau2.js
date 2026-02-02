7002f210343344afe596f88730bb63433ab7d938
hieusleep070610 <hieusleep070610@gmail.com>
2026-02-02 13:41:08 +0700

    Refactor FizzBuzz logic and sum calculation
    Removed sum calculation for FizzBuzz and adjusted logging.

--- Session 4-dh/cau2.js
+++ Session 4-dh/cau2.js
@@ -3,7 +3,6 @@ let sumFizz = 0;
 for(i = 1; i<= 50;i++){
     if( i % 3 == 0 && i % 5 == 0){
         console.log(`FizzBuzz`);
-        sumFizz += i;
     } else if (i % 5 == 0){
         console.log(`Buzz`);
     } else if (i % 3 == 0){
@@ -13,4 +12,5 @@ for(i = 1; i<= 50;i++){
         console.log(`${i}`);
     }
 }
+
 console.log(`Tổng các số chia hết cho 3 là ${sumFizz}`);
